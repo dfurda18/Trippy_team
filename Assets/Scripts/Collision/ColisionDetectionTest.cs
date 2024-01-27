73e1ca6a64743eb0db3a0a50823ec136664c77e1
Justin Rioux <122111862+3Rioux@users.noreply.github.com>
2024-01-27 11:55:10 -0400

    Collision Stumble Working Script
    Collision Stumble Working Script BUT the player does not stop running. Just a quick save before changing movement speed.

--- Assets/Scripts/Collision/ColisionDetectionTest.cs
+++ Assets/Scripts/Collision/ColisionDetectionTest.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class ColisionDetectionTest : MonoBehaviour
 {
     [SerializeField] private string colisionTag = "Player";
     Rigidbody rb;
+    
+    /* 
+     * Animation Trigger Variables
+     */
+    [SerializeField] private UnityEvent FastRunAnimationTrigger;
+
+    //Access the playermove script to stop the default movement speed 
+    //public PlayerMove jr_pm_instance;
+    //private static float jr_pm_Default_Speed;
+    //private static float jr_Fall_Stop_Speed = 0f;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+        //jr_pm_instance = GetComponent<PlayerMove>();
+        //jr_pm_Default_Speed = jr_pm_instance.speed;
         rb = GetComponent<Rigidbody>();
     }
 
@@ -23,7 +38,24 @@ public class ColisionDetectionTest : MonoBehaviour
     {
         if (collision.collider.tag == colisionTag)
         {
-            Debug.Log("Colision!");
-        }
-    }
+            if (FastRunAnimationTrigger == null)
+            {
+                print("ColisionDetectionTest was triggered but FastRunAnimationTrigger was NULL");
+            } else 
+            {
+                // first stop the movement speed of the player 
+                //jr_pm_instance.speed = jr_Fall_Stop_Speed; // set the PlayerMove script speed to zero
+
+
+                // Run the Stumble Animation 
+                FastRunAnimationTrigger.Invoke();//trigger the trigger in the animation 
+                Debug.Log("Colision fall triggered");
+
+
+                // after animation is done start moving again with the saved default speed 
+                //jr_pm_instance.speed = jr_pm_Default_Speed;
+
+            }//end if else 
+        }//end outer if 
+    }//end on collision enter
 }
